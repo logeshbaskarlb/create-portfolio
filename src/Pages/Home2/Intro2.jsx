79e5a6244f895813388d61add70d472defa1a4db
logeshbaskarlb <logeshbaskar007@gmail.com>
2024-01-29 19:51:21 +0530

    UI

--- src/Pages/Home2/Intro2.jsx
+++ src/Pages/Home2/Intro2.jsx
@@ -8,8 +8,8 @@ function Intro2() {
     const { firstName ,lastName , welcomeText , description , caption } = intro
 
   return (
-    <div className='mx-20 h-[70vh] bg-fourth  flex flex-col items-start justify-center gap-5 py-10 '>
-    <h1 className='text-fifth'>{welcomeText || '' }</h1>
+    <div className='mx-20 h-[70vh] bg-fourth flex flex-col items-start justify-center gap-5 py-10 '>
+    <h1 className='text-fifth text-xl'>{welcomeText || '' }</h1>
     <h1 className='text-7xl sm:text-4xl text-sixth   font-semibold'>
         {firstName || ''} {lastName || ''}
     </h1>
